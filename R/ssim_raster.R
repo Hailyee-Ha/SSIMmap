4660eb78191f9300683c331295e799d3d74b9b32
Hailyee-Ha <hha24@uwo.ca>
2023-06-08 17:40:18 -0400

    na.rm=TRUE

--- R/ssim_raster.R
+++ R/ssim_raster.R
@@ -25,8 +25,8 @@ ssim_raster<- function(img1, img2, global=TRUE, w=3,k1=NULL,k2=NULL) {
 
 
   #set constants
-  l <- max(terra::global(img1, fun= max), terra::global(img2, fun= max))
-  globalMin <- abs(min(terra::global(img1, fun= min), terra::global(img2, fun= min)))
+  l <- max(terra::global(img1, fun= max,na.rm=TRUE), terra::global(img2, fun= max, na.rm=TRUE))
+  globalMin <- abs(min(terra::global(img1, fun= min,na.rm=TRUE), terra::global(img2, fun= min,na.rm=TRUE)))
   l <- l - globalMin
 
   if(is.null(k1)){
